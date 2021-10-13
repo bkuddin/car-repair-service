0a788331bb0e80e770b89f3c8be1555904b09e8a
BK Uddin <1bkhossain@gmail.com>
2021-10-13 19:27:18 +0600

    Made fake data & converted to JSON

---
+++ src/fakeData/expertsFake.js
@@ -0,0 +1,26 @@
+const experts = [
+  {
+    id: 1,
+    img: "https://i.ibb.co/k384rVw/mechanic-1.jpg",
+    name: "Andrew Smith",
+    expertize: "-Engine Expert-",
+  },
+  {
+    id: 2,
+    img: "https://i.ibb.co/jZY7rP6/mechanic-2.jpg",
+    name: "John Anderson",
+    expertize: "-Polish Expert-",
+  },
+  {
+    id: 3,
+    img: "https://i.ibb.co/cx3nSjc/mechanic-3.jpg",
+    name: "Zakaria Smith",
+    expertize: "-Coloring Expert-",
+  },
+  {
+    id: 4,
+    img: "https://i.ibb.co/q72z5yY/mechanic-4.jpg",
+    name: "Sakib Anderson",
+    expertize: "-Alrounder Expert-",
+  },
+];
---
+++ src/fakeData/servicesFake.js
@@ -0,0 +1,50 @@
+const services = [
+  {
+    id: 1,
+    name: "ENGINE DIAGNOSTIC",
+    price: "300",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
+    img: "https://i.ibb.co/JqjXhYr/1.jpg",
+  },
+  {
+    id: 2,
+    name: "WHEEL ALIGNMENT",
+    price: "100",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
+    img: "https://i.ibb.co/WH8JKZH/2.jpg",
+  },
+  {
+    id: 3,
+    name: "OIL CHANGING",
+    price: "150",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
+    img: "https://i.ibb.co/DPdXGRJ/3.jpg",
+  },
+  {
+    id: 4,
+    name: "BRAKE REPARING",
+    price: "180",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
+    img: "https://i.ibb.co/JtPRt10/4.jpg",
+  },
+  {
+    id: 5,
+    name: "WASH AND GLASSING",
+    price: "100",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa.",
+    img: "https://i.ibb.co/pbb3kkn/5.jpg",
+  },
+  {
+    id: 6,
+    name: "COMPLETE ANALYSIS",
+    price: "300",
+    description:
+      "Lorem ipsum dolor sit amet, consectetu radipisi cing elitBeatae autem aperiam nequ quaera molestias voluptatibus harum ametipsa",
+    img: "https://i.ibb.co/qssgLY0/6.jpg",
+  },
+];
